d84102ce06343ac9c529e6821e2df97498b820bc
Jacob <jacobm3@gmail.com>
2020-07-06 19:22:56 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,6 +6,6 @@ data "vault_aws_access_credentials" "creds" {
 provider "aws" {
   access_key = data.vault_aws_access_credentials.creds.access_key
   secret_key = data.vault_aws_access_credentials.creds.secret_key
-  token = data.vault_aws_access_credentials.creds.token
+  token = data.vault_aws_access_credentials.creds.security_token
   region = "us-east-1"
 }
