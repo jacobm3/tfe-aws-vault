a9c1bcdadef5effac030faf36f69cc2141aaab1b
Jacob <jacobm3@gmail.com>
2020-07-07 11:20:16 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,14 +5,14 @@ provider "vault" {
 
 data "vault_aws_access_credentials" "creds" {
   backend = "aws"
-  role    = "s3-iam-role"
-  type = "creds"
-  #type    = "sts"
+  role    = "s3-sts-role"
+  #type = "creds"
+  type    = "sts"
 }
 
 provider "aws" {
   access_key = data.vault_aws_access_credentials.creds.access_key
   secret_key = data.vault_aws_access_credentials.creds.secret_key
-  #token = data.vault_aws_access_credentials.creds.security_token
+  token = data.vault_aws_access_credentials.creds.security_token
   region = "us-east-1"
 }
