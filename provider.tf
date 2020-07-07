af3d2e741a8571b49649fc5953fd190213da39d1
Jacob <jacobm3@gmail.com>
2020-07-06 19:26:24 -0500

    test - manually entered dyn creds as env vars

--- provider.tf
+++ provider.tf
@@ -4,8 +4,8 @@ data "vault_aws_access_credentials" "creds" {
 }
 
 provider "aws" {
-  access_key = data.vault_aws_access_credentials.creds.access_key
-  secret_key = data.vault_aws_access_credentials.creds.secret_key
-  token = data.vault_aws_access_credentials.creds.security_token
+  #access_key = data.vault_aws_access_credentials.creds.access_key
+  #secret_key = data.vault_aws_access_credentials.creds.secret_key
+  #token = data.vault_aws_access_credentials.creds.security_token
   region = "us-east-1"
 }
