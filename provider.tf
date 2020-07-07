8b38f27c24d4a770445a7ac60e03a9693c3f6b1a
Jacob <jacobm3@gmail.com>
2020-07-06 19:15:34 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,9 @@
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "s3-role"
+}
+
+provider "aws" {
+  access_key = "${data.vault_aws_access_credentials.creds.access_key}"
+  secret_key = "${data.vault_aws_access_credentials.creds.secret_key}"
+}
