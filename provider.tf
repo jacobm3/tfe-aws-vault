71743eba38cee89f0ed39fd38925dac41986feb6
Jacob Martinson <jacobm3@gmail.com>
2020-07-07 11:50:38 -0500

    Restructure provider.tf

--- provider.tf
+++ provider.tf
@@ -3,17 +3,30 @@ provider "vault" {
   namespace = "tfe-aws-vault"
 }
 
+# IAM user
 data "vault_aws_access_credentials" "creds" {
   backend = "aws"
   role    = "s3-iam-role"
-  #role    = "s3-sts-role"
   type = "creds"
-  #type    = "sts"
 }
 
 provider "aws" {
   access_key = data.vault_aws_access_credentials.creds.access_key
   secret_key = data.vault_aws_access_credentials.creds.secret_key
-  #token = data.vault_aws_access_credentials.creds.security_token
   region = "us-east-1"
 }
+
+
+# # STS
+# data "vault_aws_access_credentials" "creds" {
+#   backend = "aws"
+#   role    = "s3-sts-role"
+#   type    = "sts"
+# }
+
+# provider "aws" {
+#   access_key = data.vault_aws_access_credentials.creds.access_key
+#   secret_key = data.vault_aws_access_credentials.creds.secret_key
+#   token = data.vault_aws_access_credentials.creds.security_token
+#   region = "us-east-1"
+# }
